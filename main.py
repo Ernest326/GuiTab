7695a95da44718af7320bd04d39c0a913070f02a
Ernest <ernest326648@gmail.com>
2025-05-01 10:43:29 +0100

    Brainstorming planning

---
+++ main.py
@@ -0,0 +1,7 @@
+# IDEAS
+
+# Try to get all the pitches in real time using some sort of FFT (Graph out using matplotlib)
+# Convert the pitches/frequencies into notes
+# Map out the notes to MIDI/time
+# Use ML to predict tab patterns based on the notes/frequencies
+# Train and analyze the model, record data such as chords, notes, patterns, etc.
